2573ecbd49222a9f65fe4d367fe54bc76fa2f84f
Rohit Kumar <rohitkb5674@outlook.com>
2026-08-09 03:09:18 +0530

    MongoDB connection

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dns from "node:dns";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    console.log("this is db call function");
+
+    const mongodbUri = process.env.MONGODB_URI;
+    if (!mongodbUri) {
+        console.error("MONGODB_URI is not set in environment variables.");
+        process.exit(1);
+    }
+
+    dns.setServers(["10.168.106.228", "1.1.1.1", "8.8.8.8"]);
+    console.log("DNS servers used for lookup:", dns.getServers());
+
+    try {
+        const connectionInstance = await mongoose.connect(`${mongodbUri}/${DB_NAME}`);
+        console.log(`\n MongoDB connected DB host ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("there is some error", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+import express from "express";
+const app = express()
+
+dotenv.config()
+
+connectDB()
+
+app.get('/', (req, res) => {
+    res.send("Hi this is backend speaking")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`this is working at port http://localhost:${process.env.PORT}`);
+})
+// import express from 'express'
+// import { DB_NAME } from "./constants";
+// const app = express()
+
+// ;(async ()=>{
+//     try {
+//         mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on('error',(error)=>{
+//             console.log("ERROR:",error);
+//             throw error;
+            
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening at port ${process.env.PORT}` );
+//                     })
+//     } catch (err) {
+//         console.error("ERROR:",err);
+//          throw err;
+        
+        
+//     }
+// })()
