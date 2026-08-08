7c724613244297690c27a400fdc159b9f7ae9795
Rohit Kumar <rohitkb5674@outlook.com>
2026-08-09 03:19:04 +0530

    more changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
-import express from "express";
-const app = express()
+import { app } from "./app.js";
+
 
 dotenv.config()
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.send("Hi this is backend speaking")
-})
+
 
 app.listen(process.env.PORT, () => {
     console.log(`this is working at port http://localhost:${process.env.PORT}`);
 })
+
 // import express from 'express'
 // import { DB_NAME } from "./constants";
 // const app = express()
