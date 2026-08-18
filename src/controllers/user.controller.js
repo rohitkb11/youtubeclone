f9f0222b6cc4995dc24730965e1f82fba1e552c5
Rohit Kumar <rohitkb5674@outlook.com>
2026-08-18 21:02:12 +0530

    register user route and controller cloudinary upload for avatar and userImage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,69 @@
 import asyncHandler from '../utils/asyncHandler.js'
-
+import ApiError from '../utils/ApiError.js'
+import {User} from '../models/user.models.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js'
+import ApiResponse from '../utils/ApiResponse.js'
 // A controller is a function or component that handles incoming requests and controls the flow of a backend operation, usually by calling the appropriate service and returning a response.
 const registerUser = asyncHandler(async (req,res,next )=>{
-res.status(200).json({
-    message:'ok'
+// get user details from frontend
+// validation - not empty
+// check if user already exists : check with username and email
+// check for images and avatar
+// if available upload them to cloudinary , avatar
+// create user object - create entry in db 
+// remove password and refresh token field from response
+// check for user creation, if response is null or not 
+// if yes return response
+const {userName, fullName , email,password }= req.body
+console.log(email);
+
+// checking empty fields
+if(
+[userName,fullName,email,password].some((field)=> (field?.trim()==='') )
+
+){
+    throw new ApiError(400,'all fields are compulsory')
+}
+
+// checking if user exists
+const existingUser = User.findOne({
+    $or : [{userName},{fullName}]
 })
+if(existingUser){
+    throw new ApiError(409, "username or email already exists")
+}
+
+const avatarLocalPath = req.files?.avatar[0]?.path
+const userImageLocalPath = req.files?.userImage[0]?.path
+if(!avatarLocalPath){
+throw new ApiError(400,"avatar is needed")
+}
+
+const avatar =  await  uploadOnCloudinary(avatarLocalPath)
+const userImage = await uploadOnCloudinary(userImageLocalPath)
+if(!avatar){
+    throw new ApiError(400,"avatar is not uploaded")
+}
+
+const user = await User.create({
+    userName,
+    email,
+    fullName,
+    avatar :  avatar.url,
+    userImage: userImage?.url || "",
+    password, 
 })
+
+const createdUser = await User.findById(user?._id).select(
+    "-password -refreshToken"
+)
+if(!createdUser){
+    throw new ApiError(500,"user could not be registered on database")
+}
+
+return res.status(201).json(
+    new ApiResponse(200,createdUser,"User registered succesfully")
+)
+
+}) 
 export {registerUser}
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -45,7 +45,7 @@ const userSchema = new Schema({
 // Arrow function not used below because of the arrow function don't have context of this i.e. which context they talking about.
 userSchema.pre('save', async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password , 10)
+    this.password = await bcrypt.hash(this.password , 10)
     next()
 })
 userSchema.methods.isPasswordCorrect = async function (password){
